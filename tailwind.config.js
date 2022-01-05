9fde2c5d34d5bfa417b38ea6bbe67f02be7b0564
Shayan <sh.zamani384@gmail.com>
2022-01-05 16:31:23 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,1 @@
-module.exports={content:["./assets/**/*.{html,js}","*.html"],darkMode:"class",theme:{extend:{screens:{tablet:"630px",laptop:"1050px"},fontFamily:{sans:["sans-serif"]},colors:{loafer:"#EDF5E1",catalinaBlue:"#05386B",pastelGreen:"#27b265",lochinvar:"#379683"}}},plugins:[]};
+module.exports={content:["./assets/**/*.{html,js}","*.html"],darkMode:"class",theme:{extend:{screens:{tablet:"630px",laptop:"1050px"},fontFamily:{sans:["sans-serif"]},colors:{loafer:"#EDF5E1",catalinaBlue:"#05386B",jungleGreen:"#27b265",pastelGreen:"#5CDB95",lochinvar:"#379683"}}},plugins:[]};
